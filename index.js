baf1defd8aa166e120dbbbc6fc2b10928c7872b0
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-05 14:29:53 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function theBeatlesPlay(arrMusicians,arrInstruments){
+  var array = [];
+  for (let i=0; i<arrMusicians.length; i++){
+    array[array.length] = arrMusicians[i] + " plays " + arrInstruments[i];
+  }
+  return array;
+}
